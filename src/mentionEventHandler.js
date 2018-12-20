58e1b2153619fc2f3e739bed9c2b3eefa67e457c
changhc <changhc84@gmail.com>
2018-12-21 00:58:08 +0800

    mention handler: check commentable first

--- src/mentionEventHandler.js
+++ src/mentionEventHandler.js
@@ -6,9 +6,13 @@ import {
 import handleInput from './handleInput';
 
 const mentionEventHandler = async (context, instance) => {
+  // check if this comment is already a child comment
+  const canComment = await checkCommentCommentable(instance.value.comment_id);
+  if (!canComment) {
+    return;
+  }
   // get post content
   const inputStr = await pagePublicContentAccess(instance.value.post_id);
-
   // get analysed content
   const params = await handleInput(
     { state: '__INIT__' },
@@ -17,15 +21,11 @@ const mentionEventHandler = async (context, instance) => {
     '0' // for comments
   );
 
-  // check if this comment is already a child comment
-  const canComment = await checkCommentCommentable(instance.value.comment_id);
-  if (canComment) {
-    // reply to her comment
-    await replyToComment(
-      instance.value.comment_id,
-      params.replies[0].content.text
-    );
-  }
+  // reply to her comment
+  await replyToComment(
+    instance.value.comment_id,
+    params.replies[0].content.text
+  );
 };
 
 export default mentionEventHandler;
